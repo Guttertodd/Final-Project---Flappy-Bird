ff21724af0c51bde91774c76446b6df0ddccc6e1
339713273 <339713273@2260I-L29-026>
2025-01-13 09:36:18 -0500

    Add project files.

---
+++ Final Project - Flappy Bird/Game1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Final_Project___Flappy_Bird
+{
+    enum Screen
+    {
+        background
+    }
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        Rectangle window;
+
+        Texture2D backgroundTexture;
+        Texture2D batmanTexture;
+        Texture2D pipeTexture;
+        
+        SpriteBatch spriteBatch;
+
+        Screen screen;
+
+        
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+
+            // TODO: use this.Content to load your game content here
+
+            backgroundTexture = Content.Load<Texture2D>("Background (2)");
+            batmanTexture = Content.Load<Texture2D>("Batman");
+            pipeTexture = Content.Load<Texture2D>("Pipe");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+
+            spriteBatch.Draw(backgroundTexture, Vector2, Color.White);
+            
+            // TODO: Add your drawing code here
+
+            
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Final Project - Flappy Bird/Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Final_Project___Flappy_Bird.Game1();
+game.Run();
